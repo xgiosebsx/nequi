99a5b38ded4eb4ca4825da358d7aaf7313dd0ede
Gxbx <gabo_pxndx@hotmail.com>
2017-12-02 16:54:34 -0500

    add  helper to switch between pages

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public string _sceneName;
+
+    public void SceneLoader()
+    {
+        SceneManager.LoadScene(_sceneName, LoadSceneMode.Additive);
+    }
+}
