dca886cd9147186fbd53a91a9fc340bbc4d64a2d
cdtcreatic <cdt@clustercreatic.com>
2017-12-03 00:17:00 -0500

    Funcionalidad Vieja

---
+++ Assets/Scripts/ControlAvatar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAvatar : MonoBehaviour {
+
+    private bool der = false;
+    private bool izq = false;
+    public GameObject objPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void controlDer()
+    { }
+
+    public void controlIzq()
+    { }
+}
