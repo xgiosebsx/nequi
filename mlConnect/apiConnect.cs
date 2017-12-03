212db6fba63cef6796c8c7e59309d218c5e57141
Gxbx <gabo_pxndx@hotmail.com>
2017-12-03 09:18:06 -0500

    add tips logic fot ML interaction

---
+++ mlConnect/apiConnect.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace mlConnect
+{
+
+    public class StringTable
+    {
+        public string[] ColumnNames { get; set; }
+        public string[,] Values { get; set; }
+    }
+
+    class apiConnect
+    {
+        static void Main(string[] args)
+        {
+            InvokeRequestResponseService().Wait();
+        }
+
+        static async Task InvokeRequestResponseService()
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+
+                    Inputs = new Dictionary<string, StringTable>() {
+                    {
+                        "input1",
+                        new StringTable()
+                        {
+                            ColumnNames = new string[] {"UserId", "TipId", "Rating", "Pertience"},
+                            Values = new string[,] {  { "10571", "1859522", "9", "1372029665" },  }
+                        }
+                    },
+                },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+                const string apiKey = "6TRDgcNodTVPK8y9M0wxM4neXfyCh/z1dmU1ygtCATNUb0jcJ+Kfz4d8LNjKx1GJCHoBdRxfXkwHf40ixzwvvQ=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/52657246467e4158b1eb3cbe896f73e5/services/7356c408bd08407baaa7bd20849ec5c4/execute?api-version=2.0&details=true");
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Result: {0}", result);
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
