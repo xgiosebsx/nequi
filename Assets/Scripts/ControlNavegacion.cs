93a4107d763c91723b4b260bf846141e4b156344
cdtcreatic <cdt@clustercreatic.com>
2017-12-03 01:16:02 -0500

    Ventana de inicio, principal y tu cuenta implementados

---
+++ Assets/Scripts/ControlNavegacion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class ControlNavegacion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void principal()
+    {
+        EditorSceneManager.LoadScene("Principal");
+    }
+
+    public void tuCuenta()
+    {
+        EditorSceneManager.LoadScene("Principal");
+    }
+
+}
